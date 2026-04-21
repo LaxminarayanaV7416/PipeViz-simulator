02f41b260c581f2c73279365b2bc4957096b5259
Jude <jblync65@gmail.com>
2026-04-20 22:07:41 -0400

    Updated App to add grid

--- pipeviz-ui/src/App.jsx
+++ pipeviz-ui/src/App.jsx
@@ -1,16 +1,33 @@
 import { useState } from 'react'
 import ZipUpload from './components/ZipUpload'
+
+import PipelineGrid from './components/PipelineGrid'
 import './App.css'
+
+const MOCK_DATA = {
+  totalCycles: 12,
+  instructions: [
+    { label: '8ab4: sub sp, sp, #0x40',      ifCycle: 1},
+    { label: '8ab8: stp x29, x30,[sp, #48]', ifCycle: 2},
+
+    { label: '8abc: add x29, sp, #0x30', ifCycle: 3},
+    { label: '8ab0: stur w0, [x29, #-20]', ifCycle: 6},
+
+    { label: '8ac4: stur w0, [x29, #-4]', ifCycle: 7},
+  ]
+}
 
 function App() {
   const [zipFile, setZipFile] = useState(null)
 
   return (
-    <section id="center">
+    <div style={{ padding: '32px' }}>
       <h1>PipeViz</h1>
       <ZipUpload onFileSelect={(file) => setZipFile(file)} />
       {zipFile && <p>Ready to simulate: {zipFile.name}</p>}
-    </section>
+
+      <PipelineGrid data={MOCK_DATA} />
+    </div>
   )
 }
 
